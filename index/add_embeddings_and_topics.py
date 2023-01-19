3a5859c0b118dfeb5b74aa22831245c7fd1ea6c1
Benjamin Anderson <andersonbcdefg@gmail.com>
2023-01-19 14:24:17 -0500

    updated code to add topics and embeddings

--- index/add_embeddings_and_topics.py
+++ index/add_embeddings_and_topics.py
@@ -5,14 +5,6 @@ from functional import seq
 import time
 import pathlib
 
-def create_prompt(text):
-    prompt = f"""
-    const input = "{text}";
-
-    // Create list of main topics/people in the input string. (Max 10)
-    const topics = [
-    """
-    return prompt.strip()
 
 def get_topics(prompt, api_key, model="code-davinci-002"):
     openai.api_key = api_key
@@ -34,27 +26,44 @@ def get_topics(prompt, api_key, model="code-davinci-002"):
         .map(lambda x: x.strip()[1:-1]).distinct().to_list()
     return topics
 
-def add_topics(chunk_file, api_key, model="code-davinci-002"):
+def add_embeddings(chunks, api_key, model="text-embedding-ada-002"):
+    
     with open(chunk_file, "r+") as f:
         chunks = json.load(f)
         for chunk in chunks:
-            prompt = create_prompt(chunk["text"])
-            chunk["topics"] = get_topics(prompt, api_key, model)
-            time.sleep(5)
-        with open(chunk_file + ".topics.json", "w+") as f:
-            json.dump(chunks, f)
+            chunk["embedding"] = openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
+        
 
-def get_embedding(text, api_key, model="text-embedding-ada-002"):
+def add_embeddings_and_topics(in_file, out_file, api_key, rate_limit=5):
     openai.api_key = api_key
-    return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
-
-def add_embeddings(chunk_file, api_key, model="text-embedding-ada-002"):
-    with open(chunk_file, "r+") as f:
+    # Open chunk file and load
+    with open(in_file, "r+") as f:
         chunks = json.load(f)
         for chunk in chunks:
-            chunk["embedding"] = get_embedding(chunk["text"], api_key, model)
-        with open(chunk_file + ".embeddings.json", "w+") as f:
+            # Add topics
+            prompt = f"""const input = "{text}";\n\n// Create list of main topics/people in the input string. (Max 10)\n\nconst topics = ["""
+            chunk["topics"] = get_topics(prompt, api_key, model)
+            # Add embedding
+            chunk["embedding"] = openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
+            time.sleep(rate_limit)
+    with open(out_file, "w+") as f:
             json.dump(chunks, f)
 
-def add_embeddings_and_topics(in_file, out_file, api_key):
+def add_embeddings_and_topics_all(metadata_file, in_dir, out_dir, api_key, rate_limit=6):
+    with open(metadata_file, "r") as f:
+        metadata = json.load(f)
+    for episode in metadata:
+        in_file = pathlib.Path(f"{input_dir}/{episode['slug']}.chunks.json")
+        out_file = pathlib.Path(f"{output_dir}/{episode['slug']}.index.json")
+        # Make sure input file exists
+        if not in_file.is_file():
+            print(f"Input file {in_file} does not exist. Skipping...")
+            continue
+        # Make sure output file doesn't exist
+        if out_file.is_file():
+            print(f"Output file {out_file} already exists. Skipping...")
+            continue
+        print(f"Adding topics and embeddings to {in_file}, output {out_file}...")
+        add_embeddings_and_topics(in_file, out_file, api_key, rate_limit)
+
 
